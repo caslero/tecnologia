83ca2d36b92101f2aeb6e086d2df910032a4fd64
carlo <carlosjperazab@gmail.com>
2023-04-23 09:53:54 -0400

    Aqui agregamos el primer commit, que es para agregar todos los archivos

---
+++ js/jacascript.js
@@ -0,0 +1,9 @@
+function Open(){
+    let ham = document.getElementById('hammm');
+    ham.classList.add('w-[120%]');
+}
+
+function Close(){
+    let ham = document.getElementById('hammm');
+    ham.classList.remove('w-[120%]');
+}
